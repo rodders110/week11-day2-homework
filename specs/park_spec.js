77710b000170310f134b8b9f6abde0e35c879986
Roddy <roddy_daly110@hotmail.com>
2018-07-24 19:37:18 +0100

    tests passed

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -4,21 +4,45 @@ const Dinosaur = require('../models/dinosaur.js');
 
 describe('Park', function() {
 
+  let park;
+  let dino1
+
   beforeEach(function () {
+    park = new Park('Jurassic Park', 12);
+    dino1 = new Dinosaur('t-rex', 'carnivore', 50);
+    dino2 = new Dinosaur('Tricerotops', 'herbivore', 100);
 
-  })
+  });
 
-  it('should have a name');
+  it('should have a name', function(){
+    assert.strictEqual(park.name, 'Jurassic Park')
+  });
 
-  it('should have a ticket price');
+  it('should have a ticket price', function(){
+    assert.strictEqual(park.ticketPrice, 12);
+  });
 
-  it('should have a collection of dinosaurs');
+  it('should have a collection of dinosaurs', function(){
+    assert.deepStrictEqual(park.dinosaurs, []);
+  });
 
-  it('should be able to add a dinosaur to its collection');
+  it('should be able to add a dinosaur to its collection', function(){
+    park.add(dino1);
+    assert.deepEqual(park.dinosaurs, [{"species": "t-rex", "diet": "carnivore", "guestsAttractedPerDay": 50}]);
+  });
 
-  it('should be able to remove a dinosaur from its collection');
+  it('should be able to remove a dinosaur from its collection', function(){
+    park.add(dino1);
+    assert.deepEqual(park.dinosaurs, [{"species": "t-rex", "diet": "carnivore", "guestsAttractedPerDay": 50}]);
+    park.remove(dino1);
+    assert.deepEqual(park.dinosaurs, []);
+  });
 
-  it('should be able to find the dinosaur that attracts the most visitors');
+  it('should be able to find the dinosaur that attracts the most visitors', function(){
+    park.add(dino1);
+    park.add(dino2);
+    assert.strictEqual(park.findPop(), 'Tricerotops');
+  });
 
   it('should be able to find all dinosaurs of a particular species');
 
